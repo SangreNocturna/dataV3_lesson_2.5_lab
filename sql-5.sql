358eca831f47fa25f1fc030bae6727ccb253f404
SangreNocturna <jtafernandez@gmail.com>
2022-09-15 17:54:59 +0200

    sql-5.sql

--- sql-5.sql
+++ sql-5.sql
@@ -10,26 +10,41 @@ use sakila;
 select max(length) as 'max_length', min(length) as 'min_length' from sakila.film;
 
 -- 4. What's the average movie duration expressed in format (hours, minutes)?
--- select avg(length) as 'average_length of the film' from sakila.film;
+SELECT AVG(length)%60 FROM sakila.film;
+-- 115,2720 Minutes --> 1,92h --> 1h 55,2720 minutes
+SELECT concat(floor((AVG(length))/60),":",round((AVG(length))%60,0)) AS "average_length" FROM sakila.film;
 
 -- 5. How many distinct (different) actors' last names are there?
  select distinct last_name from sakila.actor;
  
  -- 6. Since how many days has the company been operating (check DATEDIFF() function)?
-
+SELECT DATEDIFF ((MAX(rental_date)), (MIN(rental_date))) as 'operative_time(days)' FROM sakila.rental; 
 
 -- 7. Show rental info with additional columns month and weekday. Get 20 results
 SELECT *, month(rental_date) as month, day(rental_date) as day FROM sakila.rental
 LIMIT 20;
 
 -- 8. Add an additional column day_type with values 'weekend' and 'workday' depending on the rental day of the week
-SELECT *, month(rental_date) as month, day(rental_date) as day, rental_date as day_type FROM sakila.rental
-where rental_date = day(rental_date);
-
+SELECT *,
+CASE
+WHEN DAYNAME(rental_date) = 'Monday' then 'Workday'
+WHEN DAYNAME(rental_date) = 'Tuesday' then 'Workday'
+WHEN DAYNAME(rental_date) = 'Wednesday' then 'Workday'
+WHEN DAYNAME(rental_date) = 'Thursday' then 'Workday'
+WHEN DAYNAME(rental_date) = 'Friday' then 'Workday'
+WHEN DAYNAME(rental_date) = 'Saturday' then 'Weekend'
+WHEN DAYNAME(rental_date) = 'Sunday' then 'Weekend'
+ELSE 'No info'
+END AS 'day_type', DAYNAME(rental_date) AS 'weekday'
+FROM sakila.rental;
 
 -- 9. Get release years
 select * from sakila.film;
 select title, release_year 
+from sakila.film;
+
+select * from sakila.film;
+select count(title) as amount_of_films
 from sakila.film;
 
 -- 10. Get all films with ARMAGEDDON in the title.
@@ -46,5 +61,5 @@ ORDER BY length DESC
 limit 10;
 
 -- 13. How many films include Behind the Scenes content?
-select count(special_features) as 'Behind the Scenes' from sakila.film
-where special_features like '%Behind the Scenes';
+select count(special_features) as 'number_of_Behind_the_Scenes' from sakila.film
+where special_features like '%Behind the Scenes%';
